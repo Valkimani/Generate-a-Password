3d864d7ddbd99fa90c0718e5117bf951fbc1b6f2
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-09-22 20:43:57 -0400

    Added a series of prompts for password criteria.

--- script.js
+++ script.js
@@ -10,7 +10,43 @@ function writePassword() {
 }
 
 function generatePassword() {
+  alert("Your password needs to meet a certain criteria");
+
+  // WHEN I click the button to generate a password
+  // THEN I am presented with a series of prompts for password criteria
+  var upperCase = confirm(
+    "Do you want to use uppercase? ABCDEFHGIJKLMNOPQRSTUVWXYZ"
+  );
+
+  var lowerCase = confirm(
+    "Do you want to use lowercase? abcdefghijklmnopqrstuvwxyz"
+  );
+
+  var specialCharacters =
+    "Do you want to use special characters? !@#$%^&*()_-=+[]{}";
+
+  var numerics = "Do you want to use numerics? 0123456789";
+
+  // WHEN prompted for password criteria
+  // THEN I select which criteria to include in the password
+
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+
+  // WHEN prompted for character types to include in the password
+  // THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+  // WHEN I answer each prompt
+  // THEN my input should be validated and at least one character type should be selected
+
+  // WHEN all prompts are answered
+  // THEN a password is generated that matches the selected criteria
+
+  // WHEN the password is generated
+  // THEN the password is either displayed in an alert or written to the page
+
   return "New generated password will replace this text.";
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
